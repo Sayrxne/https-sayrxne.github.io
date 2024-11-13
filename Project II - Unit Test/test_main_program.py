86bfa3f3d4c9f4a8c51d32128902eca6d64ca65f
Sayrxne <quiet.nightowls@gmail.com>
2024-11-13 17:28:54 +0000

    valid input

---
+++ Project II - Unit Test/test_main_program.py
@@ -0,0 +1,22 @@
+#Project II - Chasyl De Guzman
+
+import unittest
+from unittest.mock import patch
+import main_program
+
+class TestMainProgram(unittest.TestCase):
+    
+    #test_main_valid_input
+
+    @patch('builtins.input', side_effect=['3', '85', '90', '95'])
+    @patch('builtins.print')
+    def test_main_valid_input(self, mock_print, mock_input):
+        main_program.main()
+        mock_print.assert_called_with('The class average is: 90.00')
+        self.assertIn(
+            unittest.mock.call('The class average is: 90.00'), 
+            mock_print.mock_calls
+        )
+
+    
+
