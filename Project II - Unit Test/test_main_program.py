24f449a6f9c7ef807de885935b035c1e0600ff7b
Sayrxne <quiet.nightowls@gmail.com>
2024-11-13 17:30:54 +0000

    invalid number of students

--- Project II - Unit Test/test_main_program.py
+++ Project II - Unit Test/test_main_program.py
@@ -17,6 +17,17 @@ class TestMainProgram(unittest.TestCase):
             unittest.mock.call('The class average is: 90.00'), 
             mock_print.mock_calls
         )
+    
+    #test_invalid_number_of_students
+
+    @patch('builtins.input', side_effect=['0', '3', '85', '90', '95'])
+    @patch('builtins.print')
+    def test_invalid_number_of_students(self, mock_print, mock_input):
+        main_program.main()
+        mock_print.assert_called_with('The class average is: 90.00')
+        self.assertIn(
+            unittest.mock.call('The class average is: 90.00'), 
+            mock_print.mock_calls
+        )
 
     
-
